0a4c16e2eb7971697556ebe46ca372aea53dcaf4
unknown <rupam.kabasi88@gmail.com>
2020-05-02 09:19:57 +1000

    adding env_veriables

--- ec2provision.ps1
+++ ec2provision.ps1
@@ -15,4 +15,6 @@ echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
 Set-AWSCredential -AccessKey AKIAZN2TGWISBQXQXD33 -SecretKey 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 -StoreAs user1
 
 New-EC2Instance -ImageId ami-0810abbfb78d37cdf -MinCount 1 -MaxCount 1 -KeyName myPSKeyPair -SecurityGroupId sg-0cf2aef034f1116d9 -InstanceType t2.micro -SubnetId subnet-7323b92b -Userdata $userData -EncodeUserData -ProfileName user1 -Region ap-southeast-2
-    
+
+$build_number= $env:BUILD_NUMBER
+write-host $build_number
