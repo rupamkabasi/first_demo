a04c4bfcdc99a0abce16a2b3f0f7f2e97f8254fe
unknown <rupam.kabasi88@gmail.com>
2020-05-02 10:08:17 +1000

    adding env_veriables

--- ec2provision.ps1
+++ ec2provision.ps1
@@ -13,7 +13,7 @@ find /var/www -type f -exec chmod 0664 {} \;
 echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
 '@
 Set-AWSCredential -AccessKey AKIAZN2TGWISBQXQXD33 -SecretKey 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 -StoreAs user1
-
+Initialize-AWSDefaults -ProfileName user1 -Region ap-southeast-2
 $ec2instance = New-EC2Instance -ImageId ami-0810abbfb78d37cdf -MinCount 1 -MaxCount 1 -KeyName myPSKeyPair -SecurityGroupId sg-0cf2aef034f1116d9 -InstanceType t2.micro -SubnetId subnet-7323b92b -Userdata $userData -EncodeUserData -ProfileName user1 -Region ap-southeast-2
 $Tag = New-Object Amazon.EC2.Model.Tag
 $Tag.Key = "Name"
