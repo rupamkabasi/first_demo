deb30f2910aed4e0519f7edc75b2221f1ee0a5d4
unknown <rupam.kabasi88@gmail.com>
2020-05-03 21:51:48 +1000

    changeing code

--- ec2teardown.ps1
+++ ec2teardown.ps1
@@ -7,3 +7,4 @@ Set-AWSCredential -AccessKey AKIAZN2TGWISBQXQXD33 -SecretKey xS0jsuR1H/EkIRyIz1A
 Initialize-AWSDefaults -ProfileName user1 -Region ap-southeast-2
 $instanceID= (Get-EC2Tag -Filter @{Name="tag:Name";Value="$build_tag"} | Select-Object ResourceId).ResourceId
 Remove-EC2Instance -InstanceId $instanceID -Force 
+write-host code changed
